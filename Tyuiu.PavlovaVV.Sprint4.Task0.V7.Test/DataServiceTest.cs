421a974ffb67ba611b9f09817e73c59d93122c9a
Валерия Павлова <lerochka_pavlova.08@mail.ru>
2025-11-05 17:38:05 +0500

    Add project files.

---
+++ Tyuiu.PavlovaVV.Sprint4.Task0.V7.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.PavlovaVV.Sprint4.Task0.V7.Lib;
+namespace Tyuiu.PavlovaVV.Sprint4.Task0.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[] array = { 9, 8, 7, 9, 5, 4, 3, 2, 3, 7 };
+            int res = ds.GetMultOddArrEl(array);
+            int result = 178605;
+            Assert.AreEqual(result, res);
+        }
+    }
+}
---
+++ Tyuiu.PavlovaVV.Sprint4.Task0.V7/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.PavlovaVV.Sprint4.Task0.V7.Lib;
+namespace Tyuiu.PavlovaVV.Sprint4.Task0.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("********************************************************************************");
+
+
+            int[] array = { 9, 8, 7, 9, 5, 4, 3, 2, 3, 7 };
+
+
+            Console.WriteLine("Массив = { 9,8,7,9,5,4,3,2,3,7}");
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("Произведение нечетных элементов массива = " + ds.GetMultOddArrEl(array));
+            Console.ReadKey();
+        }
+    }
+}
