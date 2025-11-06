d2db528f2d3eacfdaed2dcd7bac77625ed43d522
Валерия Павлова <lerochka_pavlova.08@mail.ru>
2025-11-06 22:35:17 +0500

    Task3

---
+++ Tyuiu.PavlovaVV.Sprint4.Task3.V9.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.PavlovaVV.Sprint4.Task3.V9.Lib;
+namespace Tyuiu.PavlovaVV.Sprint4.Task3.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[,] array = new int[5, 5] { { 9, 6, 4, 5, 3 },
+                                           { 7, 4, 7, 5, 3 },
+                                           { 8, 5, 9, 9, 3 },
+                                           { 7, 8, 7, 9, 3 },
+                                           { 3, 7, 3, 7, 7 } };
+            int res = ds.Calculate(array);
+            int wait = 30;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PavlovaVV.Sprint4.Task3.V9/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.PavlovaVV.Sprint4.Task3.V9.Lib;
+namespace Tyuiu.PavlovaVV.Sprint4.Task3.V9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int[,] array = new int[5, 5] { { 9, 6, 4, 5, 3 },
+                                           { 7, 4, 7, 5, 3 },
+                                           { 8, 5, 9, 9, 3 },
+                                           { 7, 8, 7, 9, 3 },
+                                           { 3, 7, 3, 7, 7 } };
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{array[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(array);
+            Console.WriteLine("Сумма элементов во втором столбце массива: " + ds.Calculate(array));
+            Console.ReadKey();
+        }
+    }
+}
