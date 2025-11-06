0be06ed61a381dce9933441ce8392e5b39ae1316
Валерия Павлова <lerochka_pavlova.08@mail.ru>
2025-11-06 12:33:12 +0500

    Task2

---
+++ Tyuiu.PavlovaVV.Sprint4.Task2.V27.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.PavlovaVV.Sprint4.Task2.V27.Lib;
+namespace Tyuiu.PavlovaVV.Sprint4.Task2.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void Valid()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 1, 7, 2, 2, 6, 2, 3, 2, 3, 5, 7, 7, 1 };
+            int res = ds.Calculate(numsArray);
+            int wait = 14;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PavlovaVV.Sprint4.Task2.V27/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.PavlovaVV.Sprint4.Task2.V27.Lib;
+namespace Tyuiu.PavlovaVV.Sprint4.Task2.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Random rnd = new Random();
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                      *");
+            Console.WriteLine("*************************************************************************");
+            int len;
+            Console.WriteLine("Введите количество элементов массива:");
+            len = Convert.ToInt32(Console.ReadLine());
+
+            int[] numsArray = new int[len];
+            for (int i = 0; i <= len - 1; i++)
+            {
+                numsArray[i] = rnd.Next(2, 7);
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.Write(numsArray[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+            int res = ds.Calculate(numsArray);
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
