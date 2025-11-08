7c8c0868bd85b6b3b5e9de1d81a18d454b0703e0
Валерия Павлова <lerochka_pavlova.08@mail.ru>
2025-11-08 12:40:01 +0500

    Task4

---
+++ Tyuiu.PavlovaVV.Sprint4.Task4.V3.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.PavlovaVV.Sprint4.Task4.V3.Lib;
+namespace Tyuiu.PavlovaVV.Sprint4.Task4.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] nums = { {7, 9, 7, 8, 6 },
+                            {4, 4, 4, 8, 6 },
+                            {7, 9, 6, 7, 6 },
+                            {7, 5, 4, 9, 8 },
+                            {9, 6, 4, 9, 8 } };
+            int wait = 85;
+            int res = ds.Calculate(nums);
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PavlovaVV.Sprint4.Task4.V3/Program.cs
@@ -0,0 +1,54 @@
+﻿using Tyuiu.PavlovaVV.Sprint4.Task4.V3.Lib;
+namespace Tyuiu.PavlovaVV.Sprint4.Task4.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите количество строк: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите количество столбцов: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matr = new int[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"Введите {i}.{j} элемент массива: ");
+                    matr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Массив: \n");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(matr[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(matr);
+
+            Console.WriteLine("Сумма нечетных элементов массива: " + res);
+
+            Console.ReadKey();
+        }
+    }
+}
